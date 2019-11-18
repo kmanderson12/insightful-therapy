1cfa3ac990c2459330e465cfe9aba913bacd6321
Kyle Anderson <kmanderson12@gmail.com>
2019-11-17 21:51:34 -0600

    mobile fixes and tidying css

--- index.js
+++ index.js
@@ -12,12 +12,10 @@ document.addEventListener("DOMContentLoaded", () => {
         // Get the target from the "data-target" attribute
         const target = el.dataset.target;
         const $target = document.getElementById(target);
-        console.log($target);
 
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         el.classList.toggle("is-active");
         $target.classList.toggle("is-active");
-        $navbarMenu.classList.toggle("is-active");
       });
     });
   }
